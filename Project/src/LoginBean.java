b2ce134e37e9c216567631c7e7a81e79878c856d
hallien <kylesulabo@gmail.com>
2016-12-15 06:13:30 +0900

    registration manage bean jsf usage update, code needed.

--- Project/src/LoginBean.java
+++ Project/src/LoginBean.java
@@ -2,4 +2,10 @@
  * Created by Yuu on 12/02/16.
  */
 public class LoginBean {
+
+    private String customerFName, customerLName, customerEmail, customerPassword;
+    public String getCustomerFName(){
+        return (customerFName);
+    };
+
 }
