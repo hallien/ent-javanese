48da263549731f5008e42548c52977b42d9b283e
hallien <kylesulabo@gmail.com>
2016-12-16 05:28:00 +0900

    registration manage bean jsf usage update, code needed.

--- Project/src/BStoreBean.java
+++ Project/src/BStoreBean.java
@@ -3,18 +3,10 @@ import javax.faces.bean.ManagedBean;
 /**
  * Created by Yuu on 11/30/16.
  */
-@ManagedBean
+@ManagedBean (name= "bstoreBean")
 public class BStoreBean {
 
 
-    private String customerFName, customerLName, customerEmail, customerPassword;
-    public String getCustomerFName()
-    {
-        return customerFName;
-    }
 
-    public void setCustomerFName(String customerFName){
-        this.customerFName = customerFName;
-    }
 
 }
--- Project/src/LoginBean.java
+++ Project/src/LoginBean.java
@@ -1,8 +1,19 @@
 /**
  * Created by Yuu on 12/02/16.
  */
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
-
+@ManagedBean (name = "loginBean")
+@SessionScoped
 public class LoginBean {
+    private String customerFName, customerLName, customerEmail, customerPassword;
+    public String getCustomerFName()
+    {
+        return customerFName;
+    }
 
+    public void setCustomerFName(String customerFName){
+        this.customerFName = customerFName;
+    }
 }
